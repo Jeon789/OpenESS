eb69216ad9b98e753c30f2bafc9a4270c94e9f3f
jan4021 <jan4021@snu.ac.kr>
2024-11-25 12:39:24 +0900

    config file modification

--- SNU_AI/anomaly_detection/AnomalyBERT/utils/config.py
+++ SNU_AI/anomaly_detection/AnomalyBERT/utils/config.py
@@ -12,40 +12,14 @@ Reference:
 import os
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
-DATASET_DIR = 'datasets/processed'
-LOG_DIR = 'logs/'
-DATA_PROPERTY_DIR = 'data/'
+DATASET_DIR = '/data/ess/data/incell/year45_preprocessed/integrated/'
+LOG_DIR = '/data/ess/output/Anomaly_Detection/logs/'
 
-
-DATASET_LIST = ['ESS_sionyu', 'ESS_panli_bank1', 'ESS_panli_bank2']
+DATASET_LIST = ['ESS_sionyu', 'ESS_panli', 'ESS_gold', 'ESS_white']
 
 TRAIN_DATASET = {}
 TEST_DATASET = {}
-TEST_LABEL = {}
 
 for data_name in DATASET_LIST:
     TRAIN_DATASET[data_name] = os.path.join(DATASET_DIR, data_name + '_train.npy')
     TEST_DATASET[data_name] = os.path.join(DATASET_DIR, data_name + '_test.npy')
-    TEST_LABEL[data_name] = os.path.join(DATASET_DIR, data_name + '_test_labels.npy')
-
-
-DATA_DIVISION = {}
-
-DEFAULT_DIVISION = {'ESS_sionyu' : 'total',
-                    'ESS_panli_bank1' : 'total',
-                    'ESS_panli_bank2' : 'total',
-                   }
-
-
-NUMERICAL_COLUMNS = {'ESS_sionyu' : range(0,5),
-                     'ESS_panli_bank1' : range(0,5),
-                     'ESS_panli_bank2' : range(0,5)
-                    }
-
-CATEGORICAL_COLUMNS = {'ESS_sionyu' : tuple(),
-                       'ESS_panli_bank1' : tuple(),
-                       'ESS_panli_bank2' : tuple()
-                      }
-
-IGNORED_COLUMNS = {}
-
